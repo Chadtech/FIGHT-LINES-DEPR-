b84b2b1755b67d1811b50387b389b01964b58179
Jelly <Jelly@victorxlr.me>
2020-05-02 19:17:16 -0300

    New branch
    rewrote server in actix

---
+++ server-actix/src/main.rs
@@ -0,0 +1,56 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use fightlines_moves::moves::Move;
+use std::collections::HashMap;
+use std::io;
+
+//
+struct Model {
+    games: HashMap<String, Game>,
+}
+
+struct Game {
+    name: String,
+    players: Vec<Player>,
+    round: i64,
+}
+struct Player {
+    name: String,
+    /// None means the player has not submitted moves for this turn
+    /// Some(...) means they have, including the possibility that
+    /// they submitted an empty list of moves.
+    moves: Option<Vec<Move>>,
+}
+
+// Responder Objects
+// GET /
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+// GET /again
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello World Again")
+}
+
+// GET /games/count, we also pass in the state
+async fn game_count(model: web::Data<Model>) -> impl Responder {
+
+    HttpResponse::Ok().body(model.games.len().to_string())
+}
+
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .data(Model {
+                games: HashMap::new(),
+            })
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+            .route("/games/count", web::get().to(game_count))
+    })
+    .bind("127.0.0.1:2943")?
+    .run()
+    .await
+}
