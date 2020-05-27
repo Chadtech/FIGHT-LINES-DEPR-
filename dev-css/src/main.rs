d079a95acf61b55c768c56c915b49a3e196d1db7
Chad Stearns <chad@humio.com>
2020-05-26 22:23:50 -0400

    A little bit of content into css dev

--- dev-css/src/main.rs
+++ dev-css/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io};
+
+fn main() -> io::Result<()> {
+    let mut entries = fs::read_dir(".")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    Ok(())
 }
